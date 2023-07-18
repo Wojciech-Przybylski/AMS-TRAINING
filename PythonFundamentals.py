b614615e73df556b55f99f7e780b59f4217ca1c7
voy <wojciech.przybylski1@hotmail.com>
2023-07-18 12:28:07 +0100

    Created a book dictionary

--- PythonFundamentals.py
+++ PythonFundamentals.py
@@ -1,4 +1,7 @@
+print("hello world")
+
 book_dictionary = { 'Harper Lee':['To Kill a Mockingbird'], 'J. R. R. Tolkien':['The Lord of the Rings'], 'George Orwell':['1984'], 'Emily Brontë':['Wuthering Heights'], 'Frank Herbert':['Dune'], 'Miguel de Cervantes':['Don Quixote'], 'Scott Fitzgerald':['The Great Gatsby'], 'Charlotte Brontë':['Jane Eyre']}
 
 author_choice = input(f"Choose from the list of authors available: ")
-print(','.join(book_dictionary[author_choice]))
+print(','.join(book_dictionary[author_choice])) 
+
