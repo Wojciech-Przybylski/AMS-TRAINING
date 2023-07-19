864d9da78c6a16333914b883c6342df718ff5152
voy <wojciech.przybylski1@hotmail.com>
2023-07-19 10:41:12 +0100

    added grade calculator

--- exercises.py
+++ exercises.py
@@ -57,5 +57,5 @@ assessment_score = input("Enter your assessment score (/50): ")
 exam_score = input("Enter your exam score (/100): ")
 
 print(
-    f"Here is the percentage score for student: {student_name} Percentage: "
+    f"Here is the percentage score for : {student_name} Percentage: "
     f"{'%.0f' % grade_calc(homework_score, assessment_score, exam_score)}")
