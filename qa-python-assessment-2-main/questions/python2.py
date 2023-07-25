0215aaec520f7e34f462b201e969f932363a37b8
voy <wojciech.przybylski1@hotmail.com>
2023-07-25 10:52:07 +0100

    completed question two

--- qa-python-assessment-2-main/questions/python2.py
+++ qa-python-assessment-2-main/questions/python2.py
@@ -63,13 +63,21 @@ print(one(("Hi-There")))
 
 
 def two(number):
-    if number % 2 != 0 and number % 1 == 0:
-        return True
+    # If given number is greater than 1
+    if number > 1:
+        # Iterate from 2 to n / 2
+        for i in range(2, int(number / 2) + 1):
+            # If number is divisible by any number between
+            # 2 and n / 2, it is not prime
+            if (number % i) == 0:
+                return False
+        else:
+            return True
     else:
         return False
 
 
-print(two(3))
+print(two(67))
 print(two(8))
 
 
