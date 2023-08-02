4a16e8c91be5ed51e7c1fa683b5dfe336d71a2a7
voy <wojciech.przybylski1@hotmail.com>
2023-08-02 16:13:45 +0100

    Javascript intro exercise

---
+++ Front-End-HTML-CSS-JavaScript/Front-End-Exercises/javascript-intro.js
@@ -0,0 +1,19 @@
+//simple console.log() statement
+console.log("Hello World");
+
+let str1 = "Welcome to JavaScript";
+
+console.log("John");
+
+console.info("Doe");
+
+console.warn("London");
+
+console.error("Virgo");
+
+let myCar = "Mercedes";
+let model = "c300"
+
+console.log(`my favourite car is ${myCar} and model is ${model}`);
+
+console.log("%c my car is is black","background-color: black; color: white; font-family:arial; font-size: 16px; font-style: bold;")
